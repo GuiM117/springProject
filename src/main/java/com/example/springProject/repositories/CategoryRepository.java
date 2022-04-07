1d91682c98812d61b1b9ccbd18d9b6391ff1ec8a
Guilherme Martins <Guim117@hotmail.com>
2022-04-07 13:12:59 +0100

    Product + Category + DB relation(Product <-> Category)

---
+++ src/main/java/com/example/springProject/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.springProject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.springProject.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
